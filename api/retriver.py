30121287079b9f4bfdc10412869764f5966a5918
Mateus-Lacerda <mlacerdam.ai@gmail.com>
2025-11-16 17:05:46 -0300

    fix: Corrige bugs no retriver

--- api/retriver.py
+++ api/retriver.py
@@ -1,10 +1,10 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.routing import APIRouter
 
 from api.state import get_naive_retriver, get_reranker_retriver
 from retrieval.naive import NaiveRetriever
 
-router = APIRouter()
+router = APIRouter(prefix="/retriver")
 
 
 @router.get("/naive")
@@ -21,3 +21,16 @@ async def reranker_retriver(
     retriver: NaiveRetriever = Depends(get_reranker_retriver),
 ):
     return await retriver.aget_context(query)
+
+
+@router.get("/describe")
+async def describe(
+    naive: NaiveRetriever = Depends(get_naive_retriver),
+):
+    if not naive.vector_store.async_collection:
+        raise HTTPException(status_code=404, detail="Vector store not found")
+    return {
+        "configuration": naive.vector_store.async_collection.configuration_json,
+        "count": await naive.vector_store.async_collection.count(),
+    }
+
--- retrieval/store.py
+++ retrieval/store.py
@@ -23,54 +23,56 @@ class ChromaVectorStore:
             raise ValueError("Async client not initialized")
         self.async_collection = await self.async_client.get_or_create_collection("memories")
 
-    def similarity_search(self, query, k, max_distance=0.7):
+    def _debug_results(self, r):
+        print(r)
+
+    def similarity_search(self, query, k, max_distance=2.0):
         if self.collection is None:
             raise ValueError("Client not initialized")
 
         emb = self.embed_model.embed_text(query)
         r = self.collection.query(
-            query_embeddings=[emb], n_results=k, include=["documents"]
+            query_embeddings=[emb], n_results=k, include=["documents", "distances"]
         )
+        self._debug_results(r)
         results = []
         if (
             not r["distances"]
             or not r["distances"][0]
-            or not r["distances"][0][0]
             or not r["documents"]
             or not r["documents"][0]
-            or not r["documents"][0][0]
         ):
             return results
 
         for i in range(len(r["distances"][0])):
             if r["distances"][0][i] > max_distance:
-                break
+                continue
             results.append(
                 r["documents"][0][i]
             )
         return results
 
-    async def asimilarity_search(self, query, k, max_distance=0.7):
+    async def asimilarity_search(self, query, k, max_distance=2.0):
         if self.async_collection is None:
             raise ValueError("Async client not initialized")
 
         emb = await self.embed_model.aembed_text(query)
         result = await self.async_collection.query(
-            query_embeddings=[emb], n_results=k, include=["documents"]
+            query_embeddings=[emb], n_results=k, include=["documents", "distances"]
         )
+        self._debug_results(result)
         results = []
         if (
             not result["distances"]
             or not result["distances"][0]
-            or not result["distances"][0][0]
             or not result["documents"]
             or not result["documents"][0]
-            or not result["documents"][0][0]
         ):
             return results
+
         for i in range(len(result["distances"][0])):
             if result["distances"][0][i] > max_distance:
-                break
+                continue
             results.append(
                 result["documents"][0][i]
             )
